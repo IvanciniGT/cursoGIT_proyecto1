73f46725e97c2b03832d8ae78a4d9b82ecc399ff
Ivan <ivan.osuna.ayuste@gmail.com>
2021-03-16 15:25:08 +0000

    Decimales

--- src/main/java/com/curso/Libreria.java
+++ src/main/java/com/curso/Libreria.java
@@ -10,7 +10,15 @@ public class Libreria  {
        return numero1 + numero2;
     }
     
+    public static double suma( double numero1, double numero2 ) {
+       return numero1 + numero2;
+    }
+    
     public static int resta( int numero1, int numero2 ) {
+       return numero1 - numero2;
+    }
+    
+    public static double resta( double numero1, double numero2 ) {
        return numero1 - numero2;
     }
     
