a1da5570c39b667446f71eefa6ff67295bfeed84
Ivan <ivan.osuna.ayuste@gmail.com>
2021-03-16 15:13:34 +0000

    Inicio del proyecto

---
+++ src/main/java/com/curso/Libreria.java
@@ -0,0 +1,19 @@
+package com.curso;
+
+/**
+ * Paso 1: Escribo el API: Application Programming Interface
+ *
+ */
+public class Libreria  {
+    
+    public static int suma( int numero1, int numero2 ) {
+       return numero1 + numero2;
+    }
+    
+    public static int resta( int numero1, int numero2 ) {
+       return numero1 - numero2;
+    }
+    
+}
+
+
